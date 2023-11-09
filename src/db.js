d9d073803972fff41004ca2e91e45be97136337e
mr_sd_jack_003 <barson0habra@gmail.com>
2023-11-09 18:45:02 +0530

    1st commit

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+
+const config = {
+    apiKey: "AIzaSyCzEGJdHgLMDLrX9JM-1Qd218CqOCDPeBE",
+    authDomain: "firechat-vue-ddb50.firebaseapp.com",
+    projectId: "firechat-vue-ddb50",
+    storageBucket: "firechat-vue-ddb50.appspot.com",
+    messagingSenderId: "458243766591",
+    appId: "1:458243766591:web:c2628b5fda534a6935bb8a",
+    measurementId: "G-VKWDYS68LN"
+}
+
+const db = firebase.initializeApp(config);
+export default db;
